03351ad17490db1bbb09249a56490aec9e218772
solid5nake <engelenburg1980@gmail.com>
2015-06-02 15:40:04 +0200

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :pins
+  
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :password, length: { in: 6..20 }
 end
